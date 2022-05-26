40993a83bc631923750021c2ed3fed7a05a62175
Jgardin875 <jess.gardin88@gmail.com>
2022-05-26 16:32:51 -0400

    added python_data_structure_manipulation_exercises.py and data_type_and_variables.py and python_exercises.ipynb

---
+++ data_type_and_variables.py
@@ -0,0 +1,48 @@
+#1
+lm = 3
+h = 3 
+bb = 3
+
+x = 3*lm + 5*bb + 1*h
+print(x)
+
+
+G = 400
+A = 380
+F = 350
+
+y = 6*G + 4*A + 10*F
+print(y)
+
+
+
+
+class_full = True/False
+student_schedule_available = True/False
+
+student_enroll = student_schedule_available and not class_full
+
+Items_bought > 2 True/False
+
+Offer_valid_date < expiration_date True/False
+
+Premium = True/False
+
+discount = offer_valid_date and (items_bought or Premium)
+
+
+
+
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+len(password) >= 5      True/False 
+
+len(username) <= 20     True/False
+
+password == username    True/False
+
+
+password != ' *' and password != '* '
+username != ' *' and username != '* '
